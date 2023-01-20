f3738ee91ce7009e6f3efeaef6b73b4ebc226dcb
Matt Miguel <75647212+jamiguel23@users.noreply.github.com>
2023-01-19 19:38:40 -0800

    added more files

---
+++ src/components/Login.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Login extends Component {
+  render() {
+    return (
+      <div>
+        Login
+        <button>Click here to login</button>
+      </div>
+    )
+  }
+}
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react'
+import Home from './Home'
+import Login from './Login'
+import Profile from './Profile'
 
 export default class Main extends Component {
   render() {
     return (
-      <div>Main</div>
+      <div>
+        Main
+        <Home />
+        <Login />
+        <Profile />
+
+      </div>
     )
   }
 }
