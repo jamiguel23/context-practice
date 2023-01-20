c0ba2261a4a5a488362e57f3c6c0c1ad8a0b37c0
Matt Miguel <75647212+jamiguel23@users.noreply.github.com>
2023-01-20 15:53:36 -0800

    commit

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,18 +1,12 @@
 import React, { Component } from 'react'
-import Home from './Home'
-import Login from './Login'
-import Profile from './Profile'
+
 
 export default class Main extends Component {
+  
+  
   render() {
     return (
-      <div>
-        Main
-        <Home />
-        <Login />
-        <Profile />
-
-      </div>
+      <div> yes hello</div>
     )
   }
 }
